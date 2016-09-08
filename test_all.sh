8de7f14df61ee6ae2fcfcf904086ddb62e93dc6d
Paul Boddie <paul@boddie.org.uk>
2016-09-08 18:07:42 +0200

    Improved testing for unresolved names in inspected programs. Tidied up the output slightly.

--- test_all.sh
+++ test_all.sh
@@ -16,16 +16,19 @@ for FILENAME in tests/* ; do
 
     echo "$FILENAME..." 1>&2
     if ! ./lplc "$FILENAME" -r ; then exit 1 ; fi
-    echo 1>&2
-
-    # Run tests with an existing cache.
-
-    echo "$FILENAME (cached)..." 1>&2
-    if ! ./lplc "$FILENAME" ; then exit 1 ; fi
-    echo 1>&2
 
     # Check for unresolved names in the cache.
 
-    if grep -e '<depends>' -r "_cache" ; then exit 1 ; fi
+    echo " (depends)..." 1>&2
+    if grep '<depends>' -r "_cache" && \
+       ! echo "$FILENAME" | grep -q '_bad[._]' ; then
+       exit 1
+    fi
+
+    # Run tests with an existing cache.
+
+    echo " (cached)..." 1>&2
+    if ! ./lplc "$FILENAME" ; then exit 1 ; fi
+    echo 1>&2
 
 done
