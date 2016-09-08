c9b578018413c218a7315ed783f55d37ac26f1b3
Paul Boddie <paul@boddie.org.uk>
2016-09-08 23:06:03 +0200

    Incorporated deduction output analysis into the test runner, also reorganising.

--- test_all.sh
+++ test_all.sh
@@ -1,5 +1,29 @@
 #!/bin/sh
 
+# Expect failure from the "bad" tests.
+
+expect_failure() {
+    return `echo "$FILENAME" | grep -q '_bad[._]'`
+}
+
+# Check deduction output for type warnings, indicating that the program contains
+# errors.
+
+check_type_warnings() {
+
+    if [ -e "_deduced/type_warnings" ] && \
+       [ `stat -c %s "_deduced/type_warnings"` -ne 0 ] && \
+       ! expect_failure ; then
+
+       echo "Type warnings in deduced information." 1>&2
+       return 1
+    fi
+
+    return 0
+}
+
+# Main program.
+
 for FILENAME in tests/* ; do
 
     # Detect tests in their own subdirectories.
@@ -21,14 +45,24 @@ for FILENAME in tests/* ; do
 
     echo " (depends)..." 1>&2
     if grep '<depends>' -r "_cache" && \
-       ! echo "$FILENAME" | grep -q '_bad[._]' ; then
+       ! expect_failure ; then
+
+       echo "Unresolved names in the cache." 1>&2
        exit 1
     fi
+
+    # Check for type warnings in deduction output.
+
+    echo " (warnings)..." 1>&2
+    if ! check_type_warnings ; then exit 1 ; fi
 
     # Run tests with an existing cache.
 
     echo " (cached)..." 1>&2
     if ! ./lplc "$FILENAME" ; then exit 1 ; fi
-    echo 1>&2
 
+    echo " (warnings)..." 1>&2
+    if ! check_type_warnings ; then exit 1 ; fi
+
+    echo 1>&2
 done
