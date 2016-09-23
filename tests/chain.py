8bc4a7e0085b0c05c7a00552eb93ec73d3c257e3
Paul Boddie <paul@boddie.org.uk>
2016-09-23 23:05:03 +0200

    Add instance attributes to test non-static object accesses.

--- tests/chain.py
+++ tests/chain.py
@@ -3,12 +3,17 @@ class C:
         class E:
             def m(self, x):
                 return x
+            n = 123
+        p = "456"
 
 def main():
     c = C
     d = C.D
     e = C.D.E
     f = C.D.E.m
+    g = C.D.E.n
+    h = C.D.p
+    i = C.D.p.__len__
     inst = e()
     method = inst.m
     return method(5)
@@ -18,6 +23,9 @@ c = C
 d = C.D
 e = C.D.E
 f = C.D.E.m
+g = C.D.E.n
+h = C.D.p
+i = C.D.p.__len__
 inst = e()
 method = inst.m
 result2 = method(5)
