7a6016205d43f0639297608fb21f6c5d68a23407
Paul Boddie <paul@boddie.org.uk>
2016-10-23 14:54:04 +0200

    Removed redundant attribute.

--- translator.py
+++ translator.py
@@ -145,7 +145,6 @@ class PredefinedConstantRef(AttrResult):
 
     def __init__(self, value):
         self.value = value
-        self.only_function_attrs = False
 
     def __str__(self):
         return self.value
