7dec3d875d7c9a0c2db9b5fa5340bd0fc64395a5
Paul Boddie <paul@boddie.org.uk>
2016-10-29 20:17:38 +0200

    Removed superfluous import and comments.

--- lib/operator/sequence.py
+++ lib/operator/sequence.py
@@ -3,7 +3,7 @@
 """
 Operator support.
 
-Copyright (C) 2010, 2013, 2015 Paul Boddie <paul@boddie.org.uk>
+Copyright (C) 2010, 2013, 2015, 2016 Paul Boddie <paul@boddie.org.uk>
 
 This program is free software; you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free Software
@@ -19,14 +19,6 @@ You should have received a copy of the GNU General Public License along with
 this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-# Necessary import since the operator module gets imported very early during
-# builtins initialisation.
-
-from __builtins__.span import slice
-
-# NOTE: The compiler should make it possible for the following functions to call
-# NOTE: the generic operator implementations with no additional call overhead.
-
 # Access and slicing functions.
 
 def getitem(a, b):
@@ -34,8 +26,6 @@ def getitem(a, b):
 
 def setitem(a, b, c):
     a.__setitem__(b, c)
-
-# NOTE: Should be able to optimise temporary instance allocations for slices.
 
 def getslice(a, b, c):
     return a.__getitem__(slice(b, c))
