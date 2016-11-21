c400226297667c3bb471537640c8c1be1eb74cea
Paul Boddie <paul@boddie.org.uk>
2016-11-21 17:05:41 +0100

    Added print statements to test results.

--- tests/consts.py
+++ tests/consts.py
@@ -2,11 +2,15 @@ def f():
     s = "test"
     m = s.__len__
     n = "test".__len__
+    print m()
+    print n()
 
 def g():
     l = [1]
     m = l.__len__
     n = [1].__len__
+    print m()
+    print n()
 
 f()
 g()
