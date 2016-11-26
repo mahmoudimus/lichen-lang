c4be0ba4f86b1933725a56990852d5ea7b85ca7a
Paul Boddie <paul@boddie.org.uk>
2016-11-26 01:25:26 +0100

    Added expected results.

--- tests/assign_sequence.py
+++ tests/assign_sequence.py
@@ -3,9 +3,9 @@ def f():
     x = l
     a, b, c = l
     d, e, f = [1, 2, 3]
-    print a, b, c
-    print d, e, f
-    print x
+    print a, b, c           # 1, 2, 3
+    print d, e, f           # 1, 2, 3
+    print x                 # [1, 2, 3]
 
 def g(x):
     l = [1, 2, 3]
@@ -17,19 +17,19 @@ def g(x):
     print n
 
 f()
-g(0)
-g(1)
+g(0)                        # [4, [1, 2, 3], 6]
+g(1)                        # [1, 2, 3]
 
 l = [1, 2, 3]
 x = l
 a, b, c = l
 d, e, f = [1, 2, 3]
-print a, b, c
-print d, e, f
-print x
+print a, b, c               # 1, 2, 3
+print d, e, f               # 1, 2, 3
+print x                     # [1, 2, 3]
 m = [4, l, 6]
 if x:
     n = l
 else:
     n = m
-print n
+print n                     # [1, 2, 3]
