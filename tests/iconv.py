32a49fdc89a011c1fbb99b5379f6ae0bef91d4df
Paul Boddie <paul@boddie.org.uk>
2016-12-12 18:43:29 +0100

    Added tests of illegal sequences when converting between encodings.

--- tests/iconv.py
+++ tests/iconv.py
@@ -1,6 +1,6 @@
 # -*- coding: ISO-8859-1 -*-
 
-from posix.iconv import Converter, EINVAL, EILSEQ
+from posix.iconv import Converter, EILSEQ
 
 to_utf8 = Converter("ISO-8859-1", "UTF-8")
 to_utf16 = Converter("ISO-8859-1", "UTF-16")
@@ -45,6 +45,38 @@ try:
     print "second:", second             # ¸Ã¥
     from_utf8.feed(second)              # should have handled the complete input
     print str(from_utf8)                # æøå
+
+    # Attempt to convert ISO-8859-1 characters as if they were UTF-8.
+
+    from_utf8.reset()
+
+    try:
+        from_utf8.feed(iso)             # should raise an exception
+    except OSError, exc:
+        if exc.value == EILSEQ:
+            print "Not UTF-8 input:", exc.arg
+        else:
+            print "OSError:", exc.value
+
+    print str(from_utf8)                #
+
+    # Attempt to convert ISO-8859-1 characters following some UTF-8 ones.
+
+    to_utf8.reset()
+    to_utf8.feed("ÆØÅ")
+    utf8_2 = str(to_utf8)
+
+    from_utf8.reset()
+
+    try:
+        from_utf8.feed(utf8_2 + iso)    # should raise an exception
+    except OSError, exc:
+        if exc.value == EILSEQ:
+            print "Not UTF-8 input:", exc.arg
+        else:
+            print "OSError:", exc.value
+
+    print str(from_utf8)                #
 
 finally:
     to_utf8.close()
