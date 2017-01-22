aba0d9304a0d7fec1b775c0809aeba4a1258d34b
Paul Boddie <paul@boddie.org.uk>
2017-01-22 01:13:31 +0100

    Updated the test of special name attributes.

--- tests/name_attribute.py
+++ tests/name_attribute.py
@@ -1,13 +1,17 @@
 import operator
 
 class C:
-    pass
+    def f(self): pass
 
 c = C()
 print c.__name__                        # C
-print c.__mname__                       # __main__
+print c.__oname__                       # __main__
 print C.__name__                        # C
-print C.__mname__                       # __main__
+print C.__oname__                       # __main__
+print c.f.__fname__                     # f
+print c.f.__oname__                     # __main__.C
+print C.f.__fname__                     # f
+print C.f.__oname__                     # __main__.C
 
 # If it were defined, operator.__name__ would be module.
 
@@ -16,4 +20,4 @@ print operator.__mname__                # operator
 # If it were defined, operator.add.__name__ would be function.
 
 print operator.add.__fname__            # add
-print operator.add.__mname__            # operator.binary
+print operator.add.__oname__            # operator.binary
