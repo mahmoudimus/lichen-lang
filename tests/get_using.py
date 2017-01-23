1e5436078e077402d8ef1a669c3d251e6905fff3
Paul Boddie <paul@boddie.org.uk>
2017-01-23 23:46:20 +0100

    Expanded the test of get_using.

--- tests/get_using.py
+++ tests/get_using.py
@@ -3,17 +3,33 @@ class C:
         self.x = 123
 
     def f(self):
-        print self
         return self.x
+
+class D:
+    pass
 
 c = C()
 f = C.f
 fn = get_using(C.f, c)
 print fn                            # __main__.C.f
 print fn()                          # 123
+
 fn = get_using(C.f, C)
 print fn                            # __main__.C.f
 try:
     print fn()                      # fails
 except UnboundMethodInvocation:
     print "fn(): method is unbound"
+
+d = D()
+try:
+    fn = get_using(C.f, d)
+except TypeError:
+    print "get_using(C.f, d): d is not compatible with C"
+
+fn = get_using(c, C.f)
+print fn                            # <__main__.C instance>
+try:
+    print fn()                      # fails
+except TypeError:
+    print "fn(): object is not callable"
