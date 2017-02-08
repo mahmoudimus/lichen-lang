81166c46ac89dd317d1490d98c678ddc510a70d0
Paul Boddie <paul@boddie.org.uk>
2017-02-09 00:37:28 +0100

    Added another test of sequence assignment.

--- tests/assign_sequence.py
+++ tests/assign_sequence.py
@@ -16,6 +16,9 @@ def g(x):
         n = m
     print n
 
+def h():
+    return 7, 8, 9
+
 f()
 g(0)                        # [4, [1, 2, 3], 6]
 g(1)                        # [1, 2, 3]
@@ -24,12 +27,18 @@ l = [1, 2, 3]
 x = l
 a, b, c = l
 d, e, f = [1, 2, 3]
-print a, b, c               # 1, 2, 3
-print d, e, f               # 1, 2, 3
+
+print a, b, c               # 1 2 3
+print d, e, f               # 1 2 3
 print x                     # [1, 2, 3]
+
 m = [4, l, 6]
 if x:
     n = l
 else:
     n = m
+
 print n                     # [1, 2, 3]
+
+a, b, c = h()
+print a, b, c               # 7 8 9
