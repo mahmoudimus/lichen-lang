480b8cb0ed6812f5694de1cb5ecb13a4c9d317c0
Paul Boddie <paul@boddie.org.uk>
2017-02-12 01:21:33 +0100

    Made use of instantiation convenience macros.

--- templates/native/common.c
+++ templates/native/common.c
@@ -1,6 +1,6 @@
 /* Common operations for native functions.
 
-Copyright (C) 2016 Paul Boddie <paul@boddie.org.uk>
+Copyright (C) 2016, 2017 Paul Boddie <paul@boddie.org.uk>
 
 This program is free software; you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free Software
@@ -29,7 +29,7 @@ this program.  If not, see <http://www.gnu.org/licenses/>.
 __attr __new_int(int i)
 {
     /* Create a new integer and mutate the __data__ attribute. */
-    __attr attr = __new(&__InstanceTable___builtins___int_int, &__builtins___int_int, sizeof(__obj___builtins___int_int));
+    __attr attr = __NEWINSTANCE(__builtins___int_int);
     attr.value->attrs[__pos___data__].intvalue = i;
     return attr;
 }
@@ -37,7 +37,7 @@ __attr __new_int(int i)
 __attr __new_str(char *s, size_t size)
 {
     /* Create a new string and mutate the __data__ and __key__ attributes. */
-    __attr attr = __new(&__InstanceTable___builtins___str_string, &__builtins___str_string, sizeof(__obj___builtins___str_string));
+    __attr attr = __NEWINSTANCE(__builtins___str_string);
     attr.value->attrs[__pos___data__].size = size;
     attr.value->attrs[__pos___data__].strvalue = s;
     attr.value->attrs[__pos___key__] = __NULL;
@@ -47,7 +47,7 @@ __attr __new_str(char *s, size_t size)
 __attr __new_list(__fragment *f)
 {
     /* Create a new list and mutate the __data__ attribute. */
-    __attr attr = __new(&__InstanceTable___builtins___list_list, &__builtins___list_list, sizeof(__obj___builtins___list_list));
+    __attr attr = __NEWINSTANCE(__builtins___list_list);
     attr.value->attrs[__pos___data__].seqvalue = f;
     return attr;
 }
